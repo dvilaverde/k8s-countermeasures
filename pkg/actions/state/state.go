89230b0a0cebfe7f49ceb1ad3c714c1e0ac70ccc
David Vilaverde <dvilaverde@gmail.com>
2023-01-19 22:16:37 -0500

    added a new package to encapsulate the in memory view of whats deployed

---
+++ pkg/actions/state/state.go
@@ -0,0 +1,126 @@
+package state
+
+import (
+	"sync"
+
+	v1alpha1 "github.com/dvilaverde/k8s-countermeasures/apis/countermeasure/v1alpha1"
+	"github.com/dvilaverde/k8s-countermeasures/pkg/manager"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type ActiveCounterMeasures map[manager.ObjectKey]*Entry
+type RunningSet map[manager.ObjectKey]struct{}
+
+type Entry struct {
+	Name           string
+	Sources        []manager.ObjectKey
+	Key            manager.ObjectKey
+	Countermeasure *v1alpha1.CounterMeasure
+	Running        bool
+}
+
+type ActionState struct {
+	measuresMux sync.RWMutex
+	// map of deployed countermeasures, keyed by ObjectKey to the CounterMeasure spec
+	counterMeasures ActiveCounterMeasures
+	// eventname to ObjectKey of the countermeasure
+	eventIndex map[string][]manager.ObjectKey
+}
+
+func NewState() *ActionState {
+	return &ActionState{
+		measuresMux:     sync.RWMutex{},
+		counterMeasures: make(ActiveCounterMeasures, 0),
+		eventIndex:      make(map[string][]manager.ObjectKey),
+	}
+}
+
+func (s *ActionState) IsRunning(key manager.ObjectKey) bool {
+	s.measuresMux.RLock()
+	defer s.measuresMux.RUnlock()
+
+	entry, ok := s.counterMeasures[key]
+	if !ok {
+		return false
+	}
+
+	return entry.Running
+}
+
+func (s *ActionState) SetRunning(key manager.ObjectKey, isRunning bool) {
+	s.measuresMux.Lock()
+	defer s.measuresMux.Unlock()
+
+	entry := s.counterMeasures[key]
+	entry.Running = isRunning
+}
+
+func (s *ActionState) IsDeployed(key manager.ObjectKey) bool {
+	s.measuresMux.Lock()
+	defer s.measuresMux.Unlock()
+
+	_, ok := s.counterMeasures[key]
+	return ok
+}
+
+func (s *ActionState) Add(countermeasure *v1alpha1.CounterMeasure, sources []manager.ObjectKey) error {
+
+	key := manager.ToKey(countermeasure.ObjectMeta)
+	if s.IsDeployed(key) {
+		// we already have something registered here, lets remove and re-add
+		s.Remove(key.NamespacedName)
+	}
+
+	s.measuresMux.Lock()
+	defer s.measuresMux.Unlock()
+
+	onEvent := countermeasure.Spec.OnEvent
+	s.counterMeasures[key] = &Entry{
+		Name:           onEvent.EventName,
+		Sources:        sources,
+		Key:            key,
+		Countermeasure: countermeasure,
+		Running:        false,
+	}
+
+	s.eventIndex[onEvent.EventName] = append(s.eventIndex[onEvent.EventName], key)
+
+	return nil
+}
+
+func (s *ActionState) Remove(name types.NamespacedName) error {
+	s.measuresMux.Lock()
+	defer s.measuresMux.Unlock()
+
+	for k := range s.counterMeasures {
+		if k.NamespacedName == name {
+			delete(s.counterMeasures, k)
+		}
+	}
+
+	return nil
+}
+
+func (s *ActionState) GetCounterMeasures(eventName string) []Entry {
+	s.measuresMux.Lock()
+	defer s.measuresMux.Unlock()
+
+	objecKeys, ok := s.eventIndex[eventName]
+	if !ok {
+		return []Entry{}
+	}
+
+	entries := make([]Entry, 0, len(objecKeys))
+	for _, objectKey := range objecKeys {
+		entry, ok := s.counterMeasures[objectKey]
+		if !ok {
+			// something is wrong here maybe log in the future but for now just panic so
+			// we can catch during development
+			panic("invalid state")
+		}
+
+		entries = append(entries, *entry)
+	}
+
+	return entries
+}
---
+++ pkg/actions/state/state_test.go
@@ -0,0 +1,147 @@
+package state
+
+import (
+	"fmt"
+	"testing"
+
+	v1alpha1 "github.com/dvilaverde/k8s-countermeasures/apis/countermeasure/v1alpha1"
+	"github.com/dvilaverde/k8s-countermeasures/pkg/manager"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestActionState_IsRunning(t *testing.T) {
+	state := NewState()
+
+	key := Key()
+	assert.False(t, state.IsRunning(key))
+
+	cm := &v1alpha1.CounterMeasure{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       key.Name,
+			Namespace:  key.Namespace,
+			Generation: key.Generation,
+		},
+		Spec: v1alpha1.CounterMeasureSpec{
+			OnEvent: v1alpha1.OnEventSpec{
+				EventName: "event1",
+			},
+		},
+	}
+
+	state.Add(cm, nil)
+	state.SetRunning(key, true)
+	assert.True(t, state.IsRunning(key))
+}
+
+func TestActionState_IsDeployed(t *testing.T) {
+	state := NewState()
+
+	key := Key()
+	assert.False(t, state.IsDeployed(key))
+
+	cm := &v1alpha1.CounterMeasure{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       key.Name,
+			Namespace:  key.Namespace,
+			Generation: key.Generation,
+		},
+		Spec: v1alpha1.CounterMeasureSpec{
+			OnEvent: v1alpha1.OnEventSpec{
+				EventName: "event1",
+			},
+		},
+	}
+
+	state.Add(cm, nil)
+	assert.True(t, state.IsDeployed(key))
+}
+
+func TestActionState_AddSources(t *testing.T) {
+	state := NewState()
+
+	key := Key()
+	assert.False(t, state.IsDeployed(key))
+
+	cm := &v1alpha1.CounterMeasure{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       key.Name,
+			Namespace:  key.Namespace,
+			Generation: key.Generation,
+		},
+		Spec: v1alpha1.CounterMeasureSpec{
+			OnEvent: v1alpha1.OnEventSpec{
+				EventName: "event1",
+			},
+		},
+	}
+
+	source := manager.ObjectKey{
+		NamespacedName: types.NamespacedName{Namespace: "sourceNs", Name: "sourceA"},
+		Generation:     5,
+	}
+	state.Add(cm, []manager.ObjectKey{source})
+
+	assert.Equal(t, 1, len(state.counterMeasures[key].Sources))
+	assert.Equal(t, "sourceA", state.GetCounterMeasures("event1")[0].Sources[0].Name)
+}
+
+func TestActionState_Remove(t *testing.T) {
+	state := NewState()
+
+	key := Key()
+	assert.False(t, state.IsDeployed(key))
+
+	cm := &v1alpha1.CounterMeasure{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       key.Name,
+			Namespace:  key.Namespace,
+			Generation: key.Generation,
+		},
+		Spec: v1alpha1.CounterMeasureSpec{
+			OnEvent: v1alpha1.OnEventSpec{
+				EventName: "event1",
+			},
+		},
+	}
+
+	state.Add(cm, nil)
+	assert.True(t, state.IsDeployed(key))
+	state.Remove(key.NamespacedName)
+	assert.False(t, state.IsDeployed(key))
+}
+
+func TestActionState_GetCounterMeasures(t *testing.T) {
+
+	state := NewState()
+
+	key := Key()
+	assert.False(t, state.IsDeployed(key))
+
+	for i := 0; i < 10; i++ {
+		cm := &v1alpha1.CounterMeasure{
+			ObjectMeta: v1.ObjectMeta{
+				Name:       fmt.Sprintf("%s-%d", key.Name, i),
+				Namespace:  key.Namespace,
+				Generation: key.Generation,
+			},
+			Spec: v1alpha1.CounterMeasureSpec{
+				OnEvent: v1alpha1.OnEventSpec{
+					EventName: fmt.Sprintf("event%d", i%2),
+				},
+			},
+		}
+
+		state.Add(cm, nil)
+	}
+
+	assert.Equal(t, 5, len(state.GetCounterMeasures("event1")))
+}
+
+func Key() manager.ObjectKey {
+	return manager.ObjectKey{
+		NamespacedName: types.NamespacedName{Namespace: "ns", Name: "name"},
+		Generation:     1,
+	}
+}
