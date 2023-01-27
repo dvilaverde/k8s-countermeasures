a9f2cd554c17edc357d3f334996afcd7d7c0b101
David Vilaverde <dvilaverde@gmail.com>
2023-01-27 00:14:54 -0500

    added basic support for an event bus using the controller runtime workqueue as a base

--- pkg/eventbus/eventbus_test.go
+++ pkg/eventbus/eventbus_test.go
@@ -3,7 +3,7 @@ package eventbus
 import (
 	"context"
 	"fmt"
-	"sync"
+	"strconv"
 	"testing"
 	"time"
 
@@ -12,29 +12,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type MockListener struct {
-	eventsMux     sync.Mutex
-	events        []events.Event
-	counter       int
-	errorAttempts int
-}
+func TestDispatcher_Subscribe(t *testing.T) {
 
-func (l *MockListener) OnEvent(event events.Event) error {
-	l.eventsMux.Lock()
-	defer l.eventsMux.Unlock()
-
-	l.counter++
-	if l.errorAttempts > 0 && l.counter < l.errorAttempts {
-		return fmt.Errorf("mocking error")
-	}
-
-	l.events = append(l.events, event)
-	return nil
-}
-
-func TestDispatcher_Dispatch(t *testing.T) {
-	listener := &MockListener{}
-	bus := NewEventBus(listener, 2)
+	bus := NewEventBus(1)
 	bus.InjectLogger(testr.New(t))
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -51,27 +31,33 @@ func TestDispatcher_Dispatch(t *testing.T) {
 		Data:       &data,
 	}
 
-	bus.EnqueueEvent(e1)
-	assert.Eventually(t, func() bool {
-		listener.eventsMux.Lock()
-		defer listener.eventsMux.Unlock()
-		return len(listener.events) == 1
-	}, time.Second*5, time.Millisecond*50, "expected the event to be dequeued")
+	consumer, err := bus.Subscribe("e1")
+	assert.NoError(t, err)
 
-	listener.eventsMux.Lock()
-	event := listener.events[0]
-	listener.eventsMux.Unlock()
+	bus.Publish("e1", e1)
+
+	bus.consumersMux.Lock()
+	assert.Equal(t, 1, len(bus.consumers))
+	bus.consumersMux.Unlock()
+
+	event := consumer.OnEventSync()
+	assert.Equal(t, e1.Name, event.Name)
+	assert.Equal(t, e1.ActiveTime, event.ActiveTime)
+	assert.Equal(t, 1, len(*event.Data))
+	assert.Equal(t, "v1", event.Data.Get("d1"))
+
+	bus.Publish("e1", e1)
+
+	event = <-consumer.OnEvent()
 	assert.Equal(t, e1.Name, event.Name)
 	assert.Equal(t, e1.ActiveTime, event.ActiveTime)
 	assert.Equal(t, 1, len(*event.Data))
 	assert.Equal(t, "v1", event.Data.Get("d1"))
 }
 
-func TestDispatcher_DispatchRetry(t *testing.T) {
-	listener := &MockListener{
-		errorAttempts: 3,
-	}
-	bus := NewEventBus(listener, 2)
+func TestDispatcher_UnSubscribe(t *testing.T) {
+
+	bus := NewEventBus(1)
 	bus.InjectLogger(testr.New(t))
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -88,30 +74,32 @@ func TestDispatcher_DispatchRetry(t *testing.T) {
 		Data:       &data,
 	}
 
-	bus.EnqueueEvent(e1)
+	consumer, err := bus.Subscribe("e1")
+	assert.NoError(t, err)
 
-	// add an invalid item to the queue to make sure the dispatcher ignores it
-	bus.workqueue.Add("should throw this out")
-	assert.Eventually(t, func() bool {
-		listener.eventsMux.Lock()
-		defer listener.eventsMux.Unlock()
-		return len(listener.events) == 1
-	}, time.Second*5, time.Millisecond*50, "expected the event to be dequeued")
+	bus.Publish("e1", e1)
 
-	listener.eventsMux.Lock()
-	event := listener.events[0]
-	listener.eventsMux.Unlock()
+	event := consumer.OnEventSync()
 	assert.Equal(t, e1.Name, event.Name)
 	assert.Equal(t, e1.ActiveTime, event.ActiveTime)
 	assert.Equal(t, 1, len(*event.Data))
 	assert.Equal(t, "v1", event.Data.Get("d1"))
+
+	consumer.UnSubscribe()
+
+	bus.Publish("e1", e1)
+	timer := time.NewTimer(time.Millisecond * 100)
+	select {
+	case event = <-consumer.OnEvent():
+		t.Fatal("Received an event when none was expected")
+	case <-timer.C:
+		println("no event")
+	}
 }
 
-func TestDispatcher_DispatchError(t *testing.T) {
-	listener := &MockListener{
-		errorAttempts: 10000000,
-	}
-	bus := NewEventBus(listener, 2)
+func TestDispatcher_SubscribeRetry(t *testing.T) {
+
+	bus := NewEventBus(1)
 	bus.InjectLogger(testr.New(t))
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -120,22 +108,79 @@ func TestDispatcher_DispatchError(t *testing.T) {
 	// start the dispatcher in a go routine so that the rest of the test can continue
 	go bus.Start(ctx)
 
-	data := make(events.EventData)
-	data["d1"] = "v1"
-	e1 := events.Event{
-		Name:       "e1",
-		ActiveTime: time.Now(),
-		Data:       &data,
+	consumer, err := bus.Subscribe("e1")
+	assert.NoError(t, err)
+
+	for i := 0; i < 20; i++ {
+		data := make(events.EventData)
+		data["d1"] = fmt.Sprintf("%d", i)
+		e1 := events.Event{
+			Name:       "e1",
+			ActiveTime: time.Now(),
+			Data:       &data,
+		}
+
+		bus.Publish("e1", e1)
+		time.Sleep(30 * time.Millisecond)
 	}
 
-	bus.EnqueueEvent(e1)
-	assert.Never(t, func() bool {
-		listener.eventsMux.Lock()
-		defer listener.eventsMux.Unlock()
-		return len(listener.events) >= 1
-	}, time.Second*2, time.Millisecond*50, "expected the event to not be propagated to the listener")
+	// wait for some retries to expire
+	time.Sleep(2 * time.Second)
 
-	listener.eventsMux.Lock()
-	assert.GreaterOrEqual(t, listener.counter, 1)
-	listener.eventsMux.Unlock()
+	count := 0
+	for e := range consumer.OnEvent() {
+		i, err := strconv.Atoi(e.Data.Get("d1"))
+		assert.NoError(t, err)
+		assert.Equal(t, count, i)
+		count++
+
+		// if we don't get the 20 events this test will time out.
+		if count >= 20 {
+			cancel()
+		}
+	}
+
+	assert.Equal(t, 20, count)
+}
+
+func TestDispatcher_SubscribeError(t *testing.T) {
+	bus := NewEventBus(1)
+	bus.InjectLogger(testr.New(t))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// start the dispatcher in a go routine so that the rest of the test can continue
+	go bus.Start(ctx)
+
+	consumer, err := bus.Subscribe("e1")
+	assert.NoError(t, err)
+
+	for i := 0; i < 20; i++ {
+		data := make(events.EventData)
+		data["d1"] = fmt.Sprintf("%d", i)
+		e1 := events.Event{
+			Name:       "e1",
+			ActiveTime: time.Now(),
+			Data:       &data,
+		}
+
+		bus.Publish("e1", e1)
+		time.Sleep(30 * time.Millisecond)
+	}
+
+	// wait for the retries to expire
+	time.Sleep(6 * time.Second)
+
+	cancel()
+
+	count := 0
+	for e := range consumer.OnEvent() {
+		i, err := strconv.Atoi(e.Data.Get("d1"))
+		assert.NoError(t, err)
+		assert.Equal(t, count, i)
+		count++
+	}
+
+	assert.Equal(t, 10, count)
 }
